6e651e2a56fe9d3f0a593ab5f27e9f618f026e93
Andre <cavallari@live.com>
2020-07-26 10:55:50 -0300

    Apply search by manufacturer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def manufacturers_options_for_select
+    manufacturers_list = PowerGenerator.distinct(:manufacturer).pluck(:manufacturer).map { |m| [m, m] }
+    options_from_collection_for_select(manufacturers_list, :first, :last)
+  end
 end

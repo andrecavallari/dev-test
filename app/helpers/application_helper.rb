af072b35892288c6bb795cc6f584916a2a45fa67
Andre <cavallari@live.com>
2020-07-26 11:38:21 -0300

    Add some new  helpers for generating options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
-  
+  def enum_options_translated(model, collection, selected = nil)
+    collection = model.send(collection).keys.map { |item| [item, t(item, scope: "enum.#{collection}")] }
+    options_from_collection_for_select(collection, :first, :last, selected)
+  end
 end
--- app/helpers/power_generator_helper.rb
+++ app/helpers/power_generator_helper.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module PowerGeneratorHelper
-  def power_generator_manufacturers_options_for_select
-    manufacturers_list = PowerGenerator.distinct(:manufacturer).pluck(:manufacturer).map { |m| [m, m] }
-    options_from_collection_for_select(manufacturers_list, :first, :last)
+  def power_generator_manufacturers_options_for_select(selected = nil)
+    manufacturers_list = PowerGenerator.distinct(:manufacturer).pluck(:manufacturer).map do |manufacturer|
+      [manufacturer, manufacturer]
+    end
+
+    options_from_collection_for_select(manufacturers_list, :first, :last, selected)
   end
 end

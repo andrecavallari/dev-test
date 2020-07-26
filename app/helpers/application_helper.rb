f1e8c7cdf5928306e328101bee3cab8d36e26820
Andre <cavallari@live.com>
2020-07-26 14:54:15 -0300

    Create sort fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     collection = model.send(collection).keys.map { |item| [item, t(item, scope: "enum.#{collection}")] }
     options_from_collection_for_select(collection, :first, :last, selected)
   end
+
+  def power_generator_sort_collection
+    attributes = %i[name price kwp manufacturer].map do |attr| 
+      [attr, t(attr, scope: 'activerecord.attributes.power_generator')]
+    end
+
+    options_from_collection_for_select(attributes, :first, :last)
+  end
 end
